5a84f00ba74da501a4d6709b3e451fab9bf2088c
adithyan0007 <adithyan039@gmail.com>
2026-05-30 20:17:50 +0530

    changed Zod Provider on routes to new method

--- backend/src/routes/logs.routes.ts
+++ backend/src/routes/logs.routes.ts
@@ -1,10 +1,13 @@
 import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import {
   PostLogController,
   GetLogController,
 } from "../controllers/log.controller.js";
 import { logSchema } from "../schemas/log.schema.js";
 export const LogRoute = async (app: FastifyInstance) => {
-  app.post("/", { schema: logSchema }, PostLogController);
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post("/", { schema: logSchema }, PostLogController);
   app.get("/", GetLogController);
 };
